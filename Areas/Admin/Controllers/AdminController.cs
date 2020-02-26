8984c9ea2e90880983ab20cd45985c8ffc640be3
Kartik Thakkar <thakkar.kartik2011@gmail.com>
2020-02-26 16:56:12 +0530

    Charts

--- Areas/Admin/Controllers/AdminController.cs
+++ Areas/Admin/Controllers/AdminController.cs
@@ -12,6 +12,31 @@ namespace WebPractice.Areas.Admin.Controllers
         // GET: Admin/Admin
         public ActionResult Index()
         {
+            ViewBag.UserCount = (from ob in dc.tblUsers select ob).ToList().Count().ToString();
+            ViewBag.ActiveUserCount = (from ob in dc.tblUsers where ob.IsActive == true select ob).ToList().Count().ToString();
+            ViewBag.ProdCount = (from ob in dc.tblProducts select ob).ToList().Count().ToString();
+            ViewBag.CatCount  = (from ob in dc.tblCategories select ob).ToList().Count().ToString();
+
+            return View();
+        }
+        public ActionResult SampleChart()
+        {
+            //ViewBag.Y = new List<int>() { 10, 24, 23, 47, 50, 36, 27, 18 };
+            //ViewBag.X = new List<string>() { "A", "B", "C", "D", "E", "F", "G", "H" };
+
+            var Users = from ob in dc.tblUsers where ob.IsActive == true select ob;
+            string[] X = new string[Users.ToList().Count];
+            int[] Y = new int[Users.ToList().Count];
+            int i = 0;
+            foreach (tblUser u in Users)
+            {
+                X[i] = u.UserID.ToString();
+                Y[i] = (from ob in dc.tblProducts where ob.UserID == u.UserID select ob).ToList().Count;
+                i++;
+            }
+            ViewBag.X = X;
+            ViewBag.Y = Y;
+
             return View();
         }
         public ActionResult List()
